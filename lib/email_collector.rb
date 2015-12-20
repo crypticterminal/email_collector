8d9dc1c24f95239b4f5d7b2d3313590fe3b7b2c9
Alexei Fedotov <alexei.fedotov@gmail.com>
2015-12-20 21:30:10 +0300

    filter_at for parsing name at domain.com

--- lib/email_collector.rb
+++ lib/email_collector.rb
@@ -2,16 +2,39 @@ require 'google-search'
 require 'logger'
 
 module EmailCollector
-  @logger = Logger.new $stdout
+  @logger = Logger.new $stderr
   @logger.debug('logger initialized')
+  
+  KEYWORDS = ['', 'mail', 'mailto', 'email', 'contacts', 'contact', 'address', 'login', 'author'];
+  DOMAINS = ['gmail.com', 'yahoo.com', 'outlook.com', 'yandex.ru', 'mail.com', 'hotspot.com']
 
-  def self.collect(searchReq, domain)
+  def self.collect(searchReq, domain = nil)
     @logger.debug("searching for #{searchReq}")
-    Google::Search::Web.new do |search|
-      search.query = searchReq
-      search.size = :large
-    end.each { |item| @logger.debug item.title }
+    @logger.debug("domain = #{domain}")
+	
+	if (domain)
+      KEYWORDS.each do |keyword|
+        search("#{searchReq} #{keyword} \"#{domain}\"") +
+          search("#{searchReq} #{keyword} \"at #{domain}\"").map { |context| context }
+      end
+	else
+      (KEYWORDS + DOMAINS).each do |keyword|
+        search("#{searchReq} #{keyword}").map { |context| context }
+      end
+	end.uniq.compact
   end
+  
+  def self.search(searchReq, domain)
+    @logger.debug("searching for #{searchReq}")
 
+    Google::Search::Web.new do |search|
+      search.query = searchReq + 
+      search.size = :large
+    end.map { |item| filter_at(item.content) }
+  end
+  
+  def self.filter_at(s)
+    s.gsub(/\s+/, ' ').gsub(/[^a-z0-9_.%+-]+[ae]t[^a-z0-9.@-]+|([_+-]+)[ae]t\1/i, '@');
+  end
 end
 
---
+++ test/test_email_collector.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'email_collector'
+
+class EmailCollectorTest < Minitest::Unit::TestCase
+
+  def test_filter_at
+    assert_equal "name@domain.com", EmailCollector.filter_at("name at domain.com")
+    assert_equal "name@domain.ru", EmailCollector.filter_at("name et domain.ru")
+    assert_equal "name@domain.moscow", EmailCollector.filter_at("name (at) domain.moscow")
+    assert_equal "name@domain.moscow", EmailCollector.filter_at("name+at+domain.moscow")
+  end
+
+end
