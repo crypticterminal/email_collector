bb5d31e1ebe8fdb7051cdd4f29fcf5d7bd1c21be
Alexei Fedotov <alexei.fedotov@gmail.com>
2015-12-22 21:25:05 +0300

    debugged collect_plain nodomain

--- lib/email_collector.rb
+++ lib/email_collector.rb
@@ -6,29 +6,38 @@ module EmailCollector
   @logger.debug('logger initialized')
   
   @size = :huge
-   
+  def self.set_size(size)
+    @size = size
+  end
   
-  KEYWORDS = ['', 'mail', 'mailto', 'email', 'contacts', 'contact', 'address', 'login', 'author'];
-  DOMAINS = ['gmail.com', 'yahoo.com', 'outlook.com', 'yandex.ru', 'mail.com', 'hotspot.com']
+  @keywords = ['', 'mail', 'mailto', 'email', 'contacts', 'contact', 'address', 'login', 'author', 'googletalk', 'gtalk', 
+    'gmail', 'googlemail', 'yahoo', 'hotspot', 'outlook', 'yandex'];
+
+  def self.set_keywords(keywords)
+    @keywords = keywords
+  end
 
   def self.collect(searchReq, domain = nil)
+    @keywords.map { |keyword| collectPlain("#{searchReq} #{keyword}", domain) }.flatten.uniq.compact
+  end
+
+  def self.collect_plain(searchReq, domain = nil)
     @logger.debug("searching for #{searchReq}")
     @logger.debug("domain = #{domain}")
 	
 	if (domain)
-      KEYWORDS.each do |keyword|
-        search("#{searchReq} #{keyword} \"#{domain}\"") +
-          search("#{searchReq} #{keyword} \"at #{domain}\"").map { |context| context }
+      search("#{searchReq} \"#{domain}\"") +
+        search("#{searchReq} \"at #{domain}\"").map do |context|
+        @logger.debug("context = #{context}")
+        context = filter_at_domain(context)
+        context.scan(/[a-z0-9._%+-]*[a-z0-9_%+]@#{Regexp.quote(domain)}/i)
       end
 	else
-      (KEYWORDS + DOMAINS).each do |keyword|
-        search("#{searchReq} #{keyword}").map { |context| context }
+      search(searchReq).map do |context|
+        @logger.debug("context = #{context}")
+        context.scan(/[a-z0-9._%+-]*[a-z0-9_%+]@(?:[a-z0-9.-]+\.)+[a-z0-9]{2,}/i)
       end
-	end.uniq.compact
-  end
-  
-  def self.setSize(size)
-    @size = size
+	end
   end
   
   def self.search(searchReq)
@@ -38,14 +47,25 @@ module EmailCollector
       search.query = searchReq
       search.size = @size
     end.map do |item|
-      @logger.debug("URI = #{item.uri}")
-      @logger.debug(item.content)
-      filter_b(filter_at(item.content)).split('...')
+      #@logger.debug("URI = #{item.uri}")
+      #@logger.debug(item.content)
+      s = filter_b(item.content)
+      s = filter_at(s)
+      filter_exclam(s).split('...')
     end.flatten
   end
 
   def self.filter_at(s)
     s.gsub(/\s+/, ' ').gsub(/[^a-z0-9_.%+-]+[ae]t[^a-z0-9.@-]+|([_+-]+)[ae]t\1/i, '@')
+  end
+  
+  def self.filter_at_domain(s, domain)
+    s.gsub(/[^a-z0-9_%+-]+#{Regexp.quote(domain)}/, '@' + domain)
+  end
+  
+  # Transform gmail!com addresses
+  def self.filter_exclam(s)
+    s.gsub(/[!:]/, '.')
   end
   
   # One can go fix google-search gem instead
--- test/test_email_collector.rb
+++ test/test_email_collector.rb
@@ -3,6 +3,10 @@ require 'email_collector'
 
 class EmailCollectorTest < Minitest::Unit::TestCase
   @@logger = Logger.new $stderr
+  
+  def get_domain(email)
+    return email.gsub(/.*@/, '')
+  end
 
   def test_filter_at
     assert_equal "name@domain.com", EmailCollector.filter_at("name at domain.com")
@@ -11,21 +15,60 @@ class EmailCollectorTest < Minitest::Unit::TestCase
     assert_equal "name@domain.moscow", EmailCollector.filter_at("name+at+domain.moscow")
   end
   
+  def test_filter_exclam
+    assert_equal "name@my.domain.com", EmailCollector.filter_exclam("name@my:domain!com")
+  end
+
   def test_filter_b
     assert_equal "name@domain.com", EmailCollector.filter_b("<b>name</b>@<b>domain.com</b>")
   end
 
-  PATTERN = ['wikipedia', 'alexei.fedotov@gmail.com']
+  AUTHOR_EMAIL = 'alexei.fedotov@gmail.com'
+  EMAILS = [AUTHOR_EMAIL]
+  PATTERNS = ['wikipedia']
   def test_search
-    EmailCollector.setSize(:small);
+    return # quick return
+    EmailCollector.set_size(:small);
     
-    PATTERN.each do |pattern|
+    (PATTERNS + EMAILS).each do |pattern|
   	  x = EmailCollector.search("\"#{pattern}\"").join('').gsub(/ /, '')
       #@@logger.debug("PATTERN = #{pattern}")
       #@@logger.debug("x = " << x)
       #@@logger.debug(x.match(/#{pattern}/i))
-      assert(x.match(/#{pattern}/i))
+      assert(x.match(/#{Regexp.quote(pattern)}/i))
+	end
+  end
+  
+  def test_collect
+    return # quick return
+    EmailCollector.set_size(:small);
+    
+    EMAILS.each do |email|
+      res = EmailCollector.collect("\"#{email}\"")
+      #@@logger.debug(res)
+  	  assert(res.include? email)
+      res = EmailCollector.collect("\"#{email}\"", get_domain(email))
+      #@@logger.debug(res)
+      assert(res.include? email)
 	end
   end
 
+  def test_collect_plain
+    # return # quick return
+    EmailCollector.set_size(:small);
+    
+    res = EmailCollector.collect_plain("openmeetings #{AUTHOR_EMAIL}", get_domain(AUTHOR_EMAIL)).flatten
+    @@logger.debug(res)
+    assert(res.include? AUTHOR_EMAIL)
+  end
+  
+  def test_collect_plain_nodomain
+    return # quick return
+    EmailCollector.set_size(:small);
+    
+    res = EmailCollector.collect_plain("openmeetings #{AUTHOR_EMAIL}").flatten
+    @@logger.debug(res)
+    assert(res.include? AUTHOR_EMAIL)
+  end
+  
 end
